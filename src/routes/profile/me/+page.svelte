17ad91d730ee41e95a1c1f082ff3c8b56392648f
Francisco Hauva <phauva@gmail.com>
2023-08-21 06:49:22 -0400

    feat: set profile page title to the user's osu! username

--- src/routes/profile/me/+page.svelte
+++ src/routes/profile/me/+page.svelte
@@ -4,6 +4,10 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<title>{data.user.osu_username}'s Profile</title>
+</svelte:head>
+
 <div class="bg-zinc-800 rounded-xl p-4">
 	<p>osu! username: <span class="text-pink-300">{data.user.osu_username}</span></p>
 	<p>osu! id: <span class="text-pink-300">{data.user.osu_id}</span></p>
