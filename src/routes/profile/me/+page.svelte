95515e922ddc88123346636625a5344f9c591ace
Francisco Hauva <phauva@gmail.com>
2023-08-21 06:35:42 -0400

    feat: add more info to profile page

--- src/routes/profile/me/+page.svelte
+++ src/routes/profile/me/+page.svelte
@@ -6,4 +6,15 @@
 
 <div class="bg-zinc-800 rounded-xl p-4">
 	<p>osu! username: <span class="text-pink-300">{data.user.osu_username}</span></p>
+	<p>osu! id: <span class="text-pink-300">{data.user.osu_id}</span></p>
+	<p>
+		discord tag: <span class={data.user.discord_username ? 'text-blue-500' : 'text-red-500'}
+			>{data.user.discord_username}</span
+		>
+	</p>
+	<p>
+		discord id: <span class={data.user.discord_id ? 'text-blue-500' : 'text-red-500'}
+			>{data.user.discord_id}</span
+		>
+	</p>
 </div>
